80769c6ffbab9eebe2e926bb7f7c6ab4804c4ad9
Harsh Dahiya <arshhdahiya@gmail.com>
2024-12-23 11:16:33 +0530

    practice solution

--- practice.cpp
+++ practice.cpp
@@ -1,87 +1,31 @@
-Date, Iteration
-Date: 12-07-2024, Iteration: 1
-Date: 14-07-2024, Iteration: 1
-Date: 14-07-2024, Iteration: 2
-Date: 14-07-2024, Iteration: 3
-Date: 14-07-2024, Iteration: 4
-Date: 14-07-2024, Iteration: 5
-Date: 14-07-2024, Iteration: 6
-Date: 14-07-2024, Iteration: 7
-Date: 14-07-2024, Iteration: 8
-Date: 14-07-2024, Iteration: 9
-Date: 14-07-2024, Iteration: 10
-Date: 14-07-2024, Iteration: 11
-Date: 14-07-2024, Iteration: 12
-Date: 15-07-2024, Iteration: 1
-Date: 15-07-2024, Iteration: 2
-Date: 15-07-2024, Iteration: 3
-Date: 15-07-2024, Iteration: 4
-Date: 15-07-2024, Iteration: 5
-Date: 15-07-2024, Iteration: 6
-Date: 15-07-2024, Iteration: 7
-Date: 15-07-2024, Iteration: 8
-Date: 15-07-2024, Iteration: 9
-Date: 15-07-2024, Iteration: 10
-Date: 16-07-2024, Iteration: 1
-Date: 16-07-2024, Iteration: 2
-Date: 16-07-2024, Iteration: 3
-Date: 16-07-2024, Iteration: 4
-Date: 16-07-2024, Iteration: 5
-Date: 16-07-2024, Iteration: 6
-Date: 16-07-2024, Iteration: 7
-Date: 16-07-2024, Iteration: 8
-Date: 16-07-2024, Iteration: 9
-Date: 18-07-2024, Iteration: 1
-Date: 18-07-2024, Iteration: 2
-Date: 18-07-2024, Iteration: 3
-Date: 18-07-2024, Iteration: 4
-Date: 18-07-2024, Iteration: 5
-Date: 18-07-2024, Iteration: 6
-Date: 18-07-2024, Iteration: 7
-Date: 18-07-2024, Iteration: 8
-Date: 18-07-2024, Iteration: 9
-Date: 18-07-2024, Iteration: 10
-Date: 20-07-2024, Iteration: 1
-Date: 20-07-2024, Iteration: 2
-Date: 20-07-2024, Iteration: 3
-Date: 20-07-2024, Iteration: 4
-Date: 20-07-2024, Iteration: 5
-Date: 20-07-2024, Iteration: 6
-Date: 20-07-2024, Iteration: 7
-Date: 20-07-2024, Iteration: 8
-Date: 22-07-2024, Iteration: 1
-Date: 22-07-2024, Iteration: 2
-Date: 22-07-2024, Iteration: 3
-Date: 22-07-2024, Iteration: 4
-Date: 22-07-2024, Iteration: 5
-Date: 22-07-2024, Iteration: 6
-Date: 22-07-2024, Iteration: 7
-Date: 22-07-2024, Iteration: 8
-Date: 22-07-2024, Iteration: 9
-Date: 23-07-2024, Iteration: 1
-Date: 23-07-2024, Iteration: 2
-Date: 23-07-2024, Iteration: 3
-Date: 23-07-2024, Iteration: 4
-Date: 23-07-2024, Iteration: 5
-Date: 23-07-2024, Iteration: 6
-Date: 23-07-2024, Iteration: 7
-Date: 24-07-2024, Iteration: 1
-Date: 24-07-2024, Iteration: 2
-Date: 25-07-2024, Iteration: 1
-Date: 25-07-2024, Iteration: 2
-Date: 25-07-2024, Iteration: 3
-Date: 25-07-2024, Iteration: 4
-Date: 25-07-2024, Iteration: 5
-Date: 25-07-2024, Iteration: 6
-Date: 25-07-2024, Iteration: 7
-Date: 25-07-2024, Iteration: 8
-Date: 27-07-2024, Iteration: 1
-Date: 27-07-2024, Iteration: 2
-Date: 27-07-2024, Iteration: 3
-Date: 27-07-2024, Iteration: 4
-Date: 27-07-2024, Iteration: 5
-Date: 27-07-2024, Iteration: 6
-Date: 28-07-2024, Iteration: 1
-Date: 28-07-2024, Iteration: 2
-Date: 28-07-2024, Iteration: 3
-Date: 28-07-2024, Iteration: 4
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int wordCount = 1;
+        int p1 = 0;
+        int p2 = 0;
+        bool isPrefix = true;
+        while(p1<sentence.length()){
+            if(p2 == searchWord.length()){
+                return wordCount;
+            }
+            else if(sentence[p1] == ' '){
+                wordCount++;
+                isPrefix = true;
+                p2 = 0;
+            }
+            else if(sentence[p1] == searchWord[p2] && isPrefix){
+                p2++;
+            }
+            else{
+                p2 = 0;
+                isPrefix = false;
+            }
+            p1++;
+
+        }
+
+        return(p2==searchWord.length()) ? wordCount : -1;
+        
+    }
+};
